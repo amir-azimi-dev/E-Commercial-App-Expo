d932ebe693ab7a351e77b967ced4c66bd8eaf0f2
Amir <azymy3354@gmail.com>
2025-10-04 21:36:31 +0330

    initiate expo project

---
+++ Mobile/screens/user/products.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const Products = () => {
+    return (
+        <View>
+            <Text>Products Screen</Text>
+        </View>
+    );
+};
+
+export default Products;
